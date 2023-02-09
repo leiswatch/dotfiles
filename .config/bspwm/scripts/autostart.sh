710695b64b3c3d18ea587ee5495ebd4b07d3bcac
leiswatch <kowalrox@gmail.com>
2023-02-09 19:32:03 +0100

    update dotfiles

--- .config/bspwm/scripts/autostart.sh
+++ .config/bspwm/scripts/autostart.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
-/usr/bin/gnome-keyring-daemon --start --components=ssh,secrets,pkcs11,gpg &
 dbus-update-activation-environment --all &
+/usr/bin/gnome-keyring-daemon -s -d -l --unlock --components=ssh,secrets,pkcs11 &
 # xset s 900 900 &
 xset r rate 300 50 &
 picom -b &
 dunst -conf /home/leiswatch/.config/dunst/dunstrc &
-nitrogen --restore &
-
+# nitrogen --restore &
+feh --bg-scale /home/leiswatch/Pictures/1028709.jpg &
 /home/leiswatch/.config/polybar/launch.sh --blocks &
 
 redshift -t 6500:3500 -l 54.3520:18.6466 &
