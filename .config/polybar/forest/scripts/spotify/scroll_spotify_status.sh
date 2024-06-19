bdfd83d014a01b30623ad4be72291440dea0458d
leiswatch <kowalrox@gmail.com>
2024-06-19 13:58:17 +0200

    TDD: 1, Me: 0

--- .config/polybar/forest/scripts/spotify/scroll_spotify_status.sh
+++ .config/polybar/forest/scripts/spotify/scroll_spotify_status.sh
@@ -2,7 +2,7 @@
 
 # see man zscroll for documentation of the following parameters
 zscroll -l 40 \
-    --delay 0.1 \
+    --delay 0.4 \
     --scroll-padding "  " \
     --match-command "$(dirname $0)/get_spotify_status.sh --status" \
     --match-text "Playing" "--scroll 1" \
