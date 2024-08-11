935da1c5d3377458c6a4ab2bd2cabe44f5771344
leiswatch <kowalrox@gmail.com>
2024-08-11 20:06:17 +0200

    Gross hack because makerbot doesn't know how to code

--- sway/.config/sway/scripts/octopi.sh
+++ sway/.config/sway/scripts/octopi.sh
@@ -5,4 +5,4 @@ killall -q octopi-notifier
 # Wait until the processes have been shut down
 while pgrep -u $UID -x octopi-notifier >/dev/null; do sleep 1; done
 
-(/bin/octopi-notifier &)  &> /dev/null
+(/usr/bin/octopi-notifier &)  &> /dev/null
