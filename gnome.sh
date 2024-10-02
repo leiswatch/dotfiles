987dd9b09401adabccd9e10c89810039cbce850d
leiswatch <kowalrox@gmail.com>
2024-10-02 08:46:04 +0200

    Update

---
+++ gnome.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# GTK Settings
+gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark'
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+gsettings set org.gnome.desktop.interface cursor-theme 'Simp1e-Dark'
+gsettings set org.gnome.desktop.interface cursor-size '24'
+gsettings set org.gnome.desktop.interface document-font-name 'Roboto Slab 10'
+gsettings set org.gnome.desktop.interface text-scaling-factor '1.0'
+gsettings set org.gnome.desktop.interface font-name 'Ubuntu 10'
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrains Mono 10'
+gsettings set org.gnome.desktop.interface font-antialisaing 'rgba'
+gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
+
+gsettings set org.gnome.shell.keybindings switch-to-application-1 []
+gsettings set org.gnome.shell.keybindings switch-to-application-2 []
+gsettings set org.gnome.shell.keybindings switch-to-application-3 []
+gsettings set org.gnome.shell.keybindings switch-to-application-4 []
+gsettings set org.gnome.shell.keybindings switch-to-application-5 []
+gsettings set org.gnome.shell.keybindings switch-to-application-6 []
+gsettings set org.gnome.shell.keybindings switch-to-application-7 []
+gsettings set org.gnome.shell.keybindings switch-to-application-8 []
+gsettings set org.gnome.shell.keybindings switch-to-application-9 []
+
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 ['<Super>1']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 ['<Super>2']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 ['<Super>3']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 ['<Super>4']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 ['<Super>5']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 ['<Super>6']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7 ['<Super>7']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8 ['<Super>8']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-9 ['<Super>9']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 ['<Super>0']
+
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-1 ['<Super><Shift>1']
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-2 ['<Super><Shift>2']
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-3 ['<Super><Shift>3']
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-4 ['<Super><Shift>4']
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-5 ['<Super><Shift>5']
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-6 ['<Super><Shift>6']
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-7 ['<Super><Shift>7']
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-8 ['<Super><Shift>8']
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-9 ['<Super><Shift>9']
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-10 ['<Super><Shift>0']
+
+gsettings set org.gnome.desktop.wm.keybindings close ['<Alt>F4', '<Super><Shift>q']
+gsettings set org.gnome.desktop.wm.keybindings move-to-center ['<Super><Shift>c']
+gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen ['<Super>f']
+gsettings set org.gnome.desktop.wm.keybindings toggle-maximized ['<Super>m']
