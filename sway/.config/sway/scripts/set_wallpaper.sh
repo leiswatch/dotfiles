95f0bbf01dfecba2b5255e4b0ab8bdddee929477
leiswatch <kowalrox@gmail.com>
2025-06-01 15:52:03 +0200

    Committed some changes

--- sway/.config/sway/scripts/set_wallpaper.sh
+++ sway/.config/sway/scripts/set_wallpaper.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 selected=$(
-    find /home/leiswatch/Pictures/Wallpapers -type f | sort | fzf --preview "sh /home/leiswatch/.config/hypr/scripts/fzf-preview.sh {}" --delimiter / --with-nth -1
+    swayimg --gallery "$HOME"/Pictures/Wallpapers
 )
 
 if [[ -z $selected ]]; then
@@ -10,9 +10,9 @@ fi
 
 escaped_path=$(echo "$selected" | sed 's/\//\\\//g')
 
-sed -i "s/\(preload =\).*/\1 $escaped_path/g" /home/leiswatch/.config/hypr/hyprpaper.conf
-sed -i "s/\(wallpaper =\).*/\1 \,$escaped_path/g" /home/leiswatch/.config/hypr/hyprpaper.conf
-sed -i "s/\(path = \).*/\1$escaped_path/g" /home/leiswatch/.config/hypr/hyprlock.conf
+sed -i "s/\(preload =\).*/\1 $escaped_path/g" "$HOME"/.config/hypr/hyprpaper.conf
+sed -i "s/\(wallpaper =\).*/\1 \,$escaped_path/g" "$HOME"/.config/hypr/hyprpaper.conf
+sed -i "s/\(path = \).*/\1$escaped_path/g" "$HOME"/.config/hypr/hyprlock.conf
 
 # sh /home/leiswatch/.config/hypr/scripts/hyprpaper.sh
 swww img "$selected" --transition-type center
